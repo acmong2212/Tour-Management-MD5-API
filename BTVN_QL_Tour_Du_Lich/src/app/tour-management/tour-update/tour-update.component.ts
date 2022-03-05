d3525688f0301b63b1e8e80d1b2ab29beb710b62
acmong2212 <depzai02x@gmail.com>
2022-03-05 13:06:08 +0700

    ver 1.1 upload file update...

--- BTVN_QL_Tour_Du_Lich/src/app/tour-management/tour-update/tour-update.component.ts
+++ BTVN_QL_Tour_Du_Lich/src/app/tour-management/tour-update/tour-update.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Tour} from "../model/tour";
 import {TourService} from "../../service/tour.service";
 import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {Category} from "../model/category";
 import {CategoryService} from "../../service/category.service";
+import {finalize} from "rxjs";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
 
 @Component({
   selector: 'app-tour-update',
@@ -14,11 +16,13 @@ export class TourUpdateComponent implements OnInit {
   categories!: Category[];
   category!: Category
 
-  tour: Tour = new Tour(0, '','' ,0, '', this.category);
+  tour: Tour = new Tour(0, '', '', 0, '', this.category);
 
   id!: number;
+
   constructor(private tourService: TourService, private activatedRoute: ActivatedRoute,
-              private router: Router, private CategoryService: CategoryService) {
+              private router: Router, private CategoryService: CategoryService,
+              private storage: AngularFireStorage) {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       this.id = Number(<string>paramMap.get('id'))
       this.tourDetail();
@@ -51,4 +55,29 @@ export class TourUpdateComponent implements OnInit {
     })
   }
 
+  //upload File
+  @ViewChild('uploadFile', {static: true})
+  public imageDom: ElementRef | undefined;
+
+  selectedImage: any | null;
+  // arrayPicture = '';
+
+  submit() {
+    if (this.selectedImage != null) {
+      const filePath = this.selectedImage.name;
+      const fileRef = this.storage.ref(filePath);
+      this.storage.upload(filePath, this.selectedImage).snapshotChanges().pipe(
+        finalize(() => (fileRef.getDownloadURL().subscribe(url => {
+          // this.arrayPicture = url;
+          this.tour.image = url;
+          console.log(url);
+        })))
+      ).subscribe();
+    }
+  }
+
+  uploadFileImg() {
+    this.selectedImage = this.imageDom?.nativeElement.files[0];
+    this.submit();
+  }
 }
